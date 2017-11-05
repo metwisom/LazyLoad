4a9ba0666d0c57dc87483151ad98030b208b402b
metwisom <metwisom@gmail.com>
2017-11-05 03:50:43 +0300

    Добавил MutationObserver

--- mSrc.js
+++ mSrc.js
@@ -1,33 +1,60 @@
 'use strict';
 
 var mSrc = {
+	attribute_name: 'mSrc',
+	interval: 30,
 	init: function(){
-		document.addEventListener("DOMContentLoaded", () => {
-			mSrc.targetNodes = document.querySelectorAll("img[mSrc]");
-			mSrc.image = new Image();
-			mSrc.image.onload= () => {
-				mSrc.targetNodes[mSrc.index].setAttribute('src', mSrc.src)
-				mSrc.targetNodes[mSrc.index].removeAttribute('mSrc')
-				mSrc.index++;
-				setTimeout(() => {mSrc.load()}, mSrc.interval)
-			}
-			mSrc.load();
+		document.addEventListener('DOMContentLoaded', () => {
+			mSrc.start();
+			mSrc.createHook();
 		});
+	},
+	start: function(){
+		mSrc.targetNodes = document.querySelectorAll('img[' + mSrc.attribute_name + ']');
+		mSrc.image = new Image();
+		mSrc.image.onload = () => {
+			mSrc.targetNodes[mSrc.index].setAttribute('src', mSrc.src)
+			mSrc.targetNodes[mSrc.index].removeAttribute(mSrc.attribute_name)
+			mSrc.index++;
+			setTimeout(() => {mSrc.load()}, mSrc.interval)
+		}
+		mSrc.load();
+	},
+	createHook: function(){
+		var BreakForMSrcStart = {};
+		var target = document.getElementsByTagName('body')[0];
+		var observer = new MutationObserver(function(mutations){
+			try{
+				mutations.forEach(function(mutation){
+					mutation.addedNodes.forEach(function(item){
+						if(item.nodeName == 'IMG'){
+							throw BreakForMSrcStart;
+						}
+					})
+				});
+			}catch (e){
+				if (e === BreakForMSrcStart){
+					mSrc.start()
+				}
+			}
+		});
+		var config = {childList: true};
+		observer.observe(target, config);
 	},
 	load: function(){
 		if(mSrc.targetNodes.length <= mSrc.index){
 			return mSrc.clear();
 		}
-		mSrc.src = mSrc.targetNodes[mSrc.index].getAttribute('mSrc');
+		mSrc.src = mSrc.targetNodes[mSrc.index].getAttribute(mSrc.attribute_name);
 		mSrc.image.src = mSrc.src;
 	},
 	clear: function(){
 		mSrc.src = null;
 		mSrc.image = null;
+		mSrc.index = 0;
 		mSrc.targetNodes = null;
 		return true;
 	},
-	interval: 30,
 	index: 0,
 	src: null,
 	image: null,
